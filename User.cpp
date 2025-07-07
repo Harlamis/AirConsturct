c8c3b2de840ea69cb63d329babf7bcd6009a6fa2
stilniysanya <stilniy.sanya.g@gmail.com>
2025-07-07 18:09:34 +0200

    fixed file writing

--- User.cpp
+++ User.cpp
@@ -3,16 +3,22 @@
 #include <fstream>
 #include <stdexcept>
 void User::File_SaveUser() {
-	std::ofstream Users("Users.txt", std::ios::in | std::ios::app);
-	if (Users.is_open()) {
-		Users << this->login << ':' << this->password << '\n';
+	std::ofstream file;
+	file.open("Users.txt", std::ios::out | std::ios::app);
+	if (file.is_open()) {
+		std::cout << "yes";
+		file << this->login << ':' << this->password << '\n';
 	}
 	else {
-		throw std::runtime_error("An error has occured while opening Users.txt file, please, check this file or create it yourself in the Resources directory.");
+		std::cout << "no";
+		//throw std::runtime_error("An error has occured while opening Users.txt file, please, check this file or create it yourself in the Resources directory.");
 	}
-	Users.close();
+	file.close();
 }
 
 int main() {
 	User user{ "login", "pass" };
+	user.File_SaveUser();
+
+	return 0;
 }
